c9d2c9e87f003963ea37fc75c75960f19baee9d1
vitor <vitorpassosbrit@gmail.com>
2024-08-02 21:34:28 -0400

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    // urlApi: 'http://157.245.132.73:8000/api/v1',
-    urlApi: 'http://localhost:8000/api/v1',
+    urlApi: 'http://157.245.132.73:8000/api/v1',
+    // urlApi: 'http://localhost:8000/api/v1',
 
 };
   
